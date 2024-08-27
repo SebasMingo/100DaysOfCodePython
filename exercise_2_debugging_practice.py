4df2d5f5a66a0d90bbfe3ca13e8e7469c1fac1c3
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-08-26 20:46:57 -0400

    Add files via upload

--- exercise_2_debugging_practice.py
+++ exercise_2_debugging_practice.py
@@ -1,3 +1,12 @@
+'''
+Codigo a debuggear
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+'''
+
+#Codigo debuageado
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the "+" sign.")
 print('e.g. print("Hello " + "world")')
