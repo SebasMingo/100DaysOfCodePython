16546226473ad96fe22e175c5c3c31bd02fd8970
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-09-23 20:47:31 -0400

    Add files via upload

---
+++ 41_Random_Module.py
@@ -0,0 +1,19 @@
+import random
+# import my_module
+
+# print(my_module.pi)
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+random_uniform= float(random.uniform(0,5))
+print(random_uniform)
+
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")
