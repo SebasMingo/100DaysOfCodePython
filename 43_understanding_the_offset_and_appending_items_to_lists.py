2a9113fa722018e19f0feb71d4dddf0727b4a09e
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-09-23 20:47:01 -0400

    Add files via upload

---
+++ 43_understanding_the_offset_and_appending_items_to_lists.py
@@ -0,0 +1,9 @@
+states_of_united_states = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connectitut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", 'Oklahoma', "New Mexico", "Arizona", "Alaska", "Hawaii" ]
+
+states_of_united_states[1] = "Pencilvania"
+
+states_of_united_states.append("Angelaland") #Agrega un valor al final de la lista
+
+states_of_united_states.extend(["Angelaland", "Jack Bauer Land"]) #Extend agrega una lista, extiende una lista con otra lista
+
+print(states_of_united_states)
