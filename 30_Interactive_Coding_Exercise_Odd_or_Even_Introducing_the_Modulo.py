5a18e812ed01c0f7453b1f4a6fcbc750110e19bc
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-09-23 20:47:47 -0400

    Add files via upload

---
+++ 30_Interactive_Coding_Exercise_Odd_or_Even_Introducing_the_Modulo.py
@@ -0,0 +1,35 @@
+
+# # 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+
+resto_del_numero = number % 2
+
+if resto_del_numero == 0:
+    print("This is an even number.")
+else:
+    print("this is an odd number.") 
+
+###########################################################################3
+#Solucion de la profe
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+
+
+
+
+
+
+
+
+
+
+
+
