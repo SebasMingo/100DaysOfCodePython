be0e107239e18ffe0a480cc3d0de09a1764432a3
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-09-24 20:09:42 -0400

    Add files via upload

---
+++ 45_1_Matrices_en_python.py
@@ -0,0 +1,19 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+print(f"{matrix[0]}\n {matrix[1]}\n {matrix[2]}\n")
+
+#Mostrar el numero 1
+
+print(f"{matrix[0][0]}")
+
+#Mostrar el numero 4
+
+print(f"{matrix[1][0]}")
+
+#Mostrar el numero 8
+
+print(f"{matrix[2][1]}")
+
+#Mostrar el numero 3
+
+print(f"{matrix[0][2]}")
