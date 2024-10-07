8881eaf3340f6ec169afe7d533a2e3111ad0b8c1
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-10-07 20:45:14 -0300

    Add files via upload

--- 51_interactive_coding_exercise_average_height.py
+++ 51_interactive_coding_exercise_average_height.py
@@ -54,4 +54,22 @@ redondeado = round(resultado)
 
 print(redondeado)
 
+################################################################################################################################################
+#Manera mas facil posible
+total_height = sum(student_heights)
+number_of_students = len(student_heights)
+average_height = round(total_height / number_of_students)
+print(average_height)
+#Respuesta de la profesora 
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(total_height)
+
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+  
+
+
 
